5b635d88fbfd8854189f78307fbd7daa58fab0f3
Daniel Hammer <daniel.hammer+oss@gmail.com>
2021-01-16 15:26:06 +0100

    https://start.spring.io/#!type=gradle-project&language=java&platformVersion=2.3.7.RELEASE&packaging=war&jvmVersion=11&groupId=com.example&artifactId=build-info-logging&name=build-info-logging&description=Demo%20project%20for%20Spring%20Boot%20build-info%20logging&packageName=com.example.build.info.logging

---
+++ src/main/java/com/example/build/info/logging/BuildInfoLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.build.info.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildInfoLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildInfoLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/build/info/logging/BuildInfoLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.build.info.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildInfoLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
